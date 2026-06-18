60c694667a84c9924b6d1b7015b55e8e1600f07b
J.J. Wang <oshukezu@gmail.com>
2026-06-19 00:09:50 +0800

    Delete config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// Tetris 全域環境變數配置
-window.TETRIS_CONFIG = {
-  CLOUD_ENABLED: true,
-  SUPABASE_URL: 'https://wapmkazqlcpdmbnzalkv.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_5qdd6GXGcZXGRwm-aAbRPw_rj_s39xp',
-  INTEGRITY_SALT: 'Tetris_School_League_Salt_2026',
-  CHALLENGE_EXPIRY_HOURS: 72
-};
